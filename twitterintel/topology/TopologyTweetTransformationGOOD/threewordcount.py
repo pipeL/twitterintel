474c3aabba820b650ae39d8aa1ca48fc381bbaee
Luís Costa <luiscosta.lf@outlook.com>
2016-09-12 11:12:53 +0100

    File Changes

---
+++ twitterintel/topology/TopologyTweetTransformationGOOD/threewordcount.py
@@ -0,0 +1,29 @@
+import pymongo
+import nltk.corpus
+
+
+from petrel import storm
+from petrel.emitter import BasicBolt 
+
+class ThreeWordCountBolt(BasicBolt):
+    def __init__(self):
+        super(ThreeWordCountBolt, self).__init__(script=__file__)
+        self.stop_words = set(nltk.corpus.stopwords.words('english'))
+        self.stop_words.update(['http', 'https', 'rt'])
+    
+    def initialize(self,conf,context):
+        self.db = pymongo.MongoClient()
+        
+    def declareOutputFields(self):
+        return[]
+
+    def process(self,tup):
+        aux='GOOD'+tup.values[1]
+        self.db[aux].threewordcole.update(
+            {'word':str(tup.values[0])},
+            {"$inc": { 'count': 1}},
+            upsert=True
+        )
+
+def run():
+    ThreeWordCountBolt().run()
